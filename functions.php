5c87981f9fd5f0b640d7c3b92a5d2a3af2b9699c
wts-thomas <thomas@wtsks.com>
2022-12-16 16:19:39 -0600

    Initial commit

---
+++ functions.php
@@ -0,0 +1,315 @@
+<?php
+
+// This is what checks the Github Repository for the latest version 
+// and gives the update notice to the Theme installed in Wordpress.
+require 'plugin-update-checker/plugin-update-checker.php';
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker(
+	'https://github.com/wts-thomas/wts-wordpress-theme-onboarding/',
+	__FILE__,
+	'wts-elementor'
+);
+
+//Set the branch that contains the stable release.
+$myUpdateChecker->setBranch('main');
+
+
+/*  Performance & Security Edits
+_____________________________________________________________________*/
+
+// REMOVES THE WORDPRESS VERSION NUMBER
+remove_action('wp_head', 'wp_generator');
+
+// REMOVE WLWMANIFEST
+remove_action('wp_head', 'wlwmanifest_link');
+
+// REMOVE RSD
+remove_action('wp_head', 'rsd_link');
+
+// CANCELS AUTO UPDATES FOR PLUGINS AND THEMES
+add_filter( 'auto_update_plugin', '__return_false' );
+add_filter( 'auto_update_theme', '__return_false' );
+
+// REMOVE AVATAR DONATION MESSAGE
+remove_action('wpua_donation_message', 'wpua_do_donation_message');
+
+// REMOVE RSS FEEDS AND LINKS
+add_action( 'do_feed', 'aioo_crunchify_perf_disable_feed', 1 );
+add_action( 'do_feed_rdf', 'aioo_crunchify_perf_disable_feed', 1 );
+add_action( 'do_feed_rss', 'aioo_crunchify_perf_disable_feed', 1 );
+add_action( 'do_feed_rss2', 'aioo_crunchify_perf_disable_feed', 1 );
+add_action( 'do_feed_atom', 'aioo_crunchify_perf_disable_feed', 1 );
+add_action( 'do_feed_rss2_comments', 'aioo_crunchify_perf_disable_feed', 1 );
+add_action( 'do_feed_atom_comments', 'aioo_crunchify_perf_disable_feed', 1 );
+
+add_action( 'feed_links_show_posts_feed', '__return_false', - 1 );
+add_action( 'feed_links_show_comments_feed', '__return_false', - 1 );
+remove_action( 'wp_head', 'feed_links', 2 );
+remove_action( 'wp_head', 'feed_links_extra', 3 );
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+// REMOVE JQUERY MIGRATE
+function remove_jquery_migrate( $scripts ) {
+   if ( ! is_admin() && isset( $scripts->registered['jquery'] ) ) {
+        $script = $scripts->registered['jquery'];
+      if ( $script->deps ) { 
+      // Check whether the script has any dependencies
+         $script->deps = array_diff( $script->deps, array( 'jquery-migrate' ) );
+      }
+   }
+ }
+add_action( 'wp_default_scripts', 'remove_jquery_migrate' );
+
+
+/*  Elementor Edits
+________________________________________________________________________*/
+
+// REMOVE GOOGLE FRONTS - ELEMENTOR
+add_filter( 'elementor/frontend/print_google_fonts', '__return_false' );
+
+// REMOVE ELEMENTOR GLOBAL STYLES
+function dequeue_elementor_global__css() {
+  wp_dequeue_style('elementor-global');
+  wp_deregister_style('elementor-global');
+}
+add_action('wp_print_styles', 'dequeue_elementor_global__css', 9999);
+
+add_action( 'init',function(){
+   remove_action( 'wp_enqueue_scripts', 'wp_enqueue_global_styles' );
+   remove_action( 'wp_footer', 'wp_enqueue_global_styles', 1 );
+   remove_action( 'wp_body_open', 'wp_global_styles_render_svg_filters' );
+} );
+
+// REMOVE GUTENBERG BLOCK LIBRARY CSS
+function smartwp_remove_wp_block_library_css(){
+   wp_dequeue_style( 'wp-block-library' );
+   wp_dequeue_style( 'wp-block-library-theme' );
+}
+add_action( 'wp_enqueue_scripts', 'smartwp_remove_wp_block_library_css' );
+
+add_filter('use_block_editor_for_post', '__return_false');
+
+function eos_dequeue_gutenberg() {
+   wp_dequeue_style( 'wp-core-blocks' );
+   wp_dequeue_style( 'wp-block-library' );
+   wp_deregister_style( 'wp-core-blocks' );
+   wp_deregister_style( 'wp-block-library' );
+}
+add_action( 'wp_print_styles', 'eos_dequeue_gutenberg' );
+
+
+// Add theme support for Featured Images
+add_theme_support( 'post-thumbnails' );
+
+/*  ADMIN DASHBOARD LINKS
+________________________________________________________________________*/
+
+// Remove Admin features from Dashboard excluding WTS users
+// for the default installation of plugins
+function wts_remove_menus(){ 
+   $current_user = wp_get_current_user(); 
+   if( !in_array( $current_user->user_email, array('thomas@wtsks.com','tanner@wtsks.com',) ) ){ 
+      /*  	remove_menu_page( 'index.php' );                        //Dashboard 	*/
+      /* 	remove_menu_page( 'edit.php' );                         //Posts		*/
+      /*  	remove_menu_page( 'upload.php' );                       //Media 		*/
+      /*  	remove_menu_page( 'edit.php?post_type=page' );          //Pages 		*/
+      /* 	remove_menu_page( 'edit-comments.php' );                //Comments	*/
+      remove_menu_page( 'themes.php' );                             //Appearance */
+      remove_menu_page( 'plugins.php' );                            //Plugins		*/
+      /* 	remove_menu_page( 'users.php' );                        //Users		*/
+      remove_menu_page( 'tools.php' );                              //Tools		*/
+      remove_menu_page( 'options-general.php' );                    //Settings 	*/
+      remove_menu_page( 'edit.php?post_type=acf-field-group' );     //ACF 	      */
+      remove_menu_page( 'cptui_main_menu' );                        //CPT UI     */
+   } 
+} 
+add_action( 'admin_menu', 'wts_remove_menus', 9999 );
+
+
+/*  DASHBOARD META BOXES - DEFAULT SCREEN OPTIONS
+________________________________________________________________________*/
+
+// Hides the other screen option meta boxes.
+// Boxes can be activated at any time by the user via Screen Options dropdown.
+add_filter( 'hidden_meta_boxes', 'custom_hidden_meta_boxes' );
+function custom_hidden_meta_boxes( $hidden ) {
+    $hidden[] = 'e-dashboard-overview';
+    $hidden[] = 'dashboard_site_health';
+    $hidden[] = 'dashboard_right_now';
+    $hidden[] = 'dashboard_activity';
+    $hidden[] = 'dashboard_quick_press';
+    $hidden[] = 'dashboard_primary';
+    $hidden[] = 'rg_forms_dashboard';
+    return $hidden;
+}
+
+
+/*  ASYNC FUNCTION FOR SCRIPTS - ENQUEUED BELOW
+________________________________________________________________________*/
+
+function site_async_scripts($url)
+{
+    if ( strpos( $url, '#asyncload') === false )
+        return $url;
+    else if ( is_admin() )
+        return str_replace( '#asyncload', '', $url );
+    else
+	return str_replace( '#asyncload', '', $url )."' async='async"; 
+    }
+add_filter( 'clean_url', 'site_async_scripts', 11, 1 );
+
+// add "#asyncload" to the end of the js file name. I.E. nameoffile-morename.js#asyncload
+
+
+/*  LOAD THEME STYLES AND SCRIPTS
+________________________________________________________________________*/
+
+function add_theme_enqueues() {
+	wp_enqueue_style( 'style', get_stylesheet_uri() );
+	wp_deregister_script('jquery');
+	wp_enqueue_script( 'jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js', array(), '3.6.1', false);
+	wp_enqueue_script( 'viewportHeight', get_template_directory_uri() . '/js/viewportHeight.js#asyncload', array ( 'jquery' ), 1, true);
+   wp_enqueue_script( 'responsiveTables', get_template_directory_uri() . '/js/responsiveTables.js#asyncload', array ( 'jquery' ), 1, true);
+}
+add_action( 'wp_enqueue_scripts', 'add_theme_enqueues' );
+
+
+// DEFER RECAPTCHA
+add_filter( 'clean_url', function( $url )
+{
+    if ( FALSE === strpos( $url, 'www.google.com/recaptcha/api.js' ) )
+    { // not our file
+        return $url;
+    }
+    // Must be a ', not "!
+    return "$url' defer='defer";
+}, 11, 1 );
+
+
+/*  SVG IMAGES
+________________________________________________________________________*/
+	
+/*  Allows the use of SVGs
+	to be uploaded to the Media Library
+__________________________________________*/
+
+define( 'ALLOW_UNFILTERED_UPLOADS', true );
+
+function cc_mime_types($mimes) {
+  $mimes['svg'] = 'image/svg+xml';
+  return $mimes;
+}
+add_filter('upload_mimes', 'cc_mime_types');
+
+// NOTE: SVG width and height functions are not required since we're using Elementor and its' SVG upload to media library functions.
+
+
+/*  LOADS ELEMENTOR TO TEMPLATE PAGES
+________________________________________________________________________*/
+
+function theme_prefix_register_elementor_locations( $elementor_theme_manager ) {
+	$elementor_theme_manager->register_all_core_location();
+
+}
+add_action( 'elementor/theme/register_locations', 'theme_prefix_register_elementor_locations' );
+
+
+/*  SUPPORT CONATACT CARD
+________________________________________________________________________*/
+
+function custom_dashboard_help() {
+   echo '
+   <div style="text-align:center;">
+       <a href="https://wtsks.com/help/" title="Contact WTS" target="_blank">
+           <img src="'.get_template_directory_uri().'/img/wts-logo_whiteback.png" alt="WTS" style="max-width:100%;width:80%;height:auto;margin:20px auto;">
+       </a>
+   </div>
+   <p>
+      Contact <a href="https://wtsks.com/help/" title="Contact WTS" target="_blank">Contact WTS</a> with questions, troubleshooting, edit for requests or alterations, or misc support you have with your custom built website.
+   </p>
+   <p><strong><a href="https://wtsks.com/help/" title="Contact WTS" target="_blank">Contact WTS</a></strong></p>
+   ';
+}
+function wts_custom_dashboard_widgets() {
+   global $wp_meta_boxes;
+   wp_add_dashboard_widget('custom_help_widget', 'Website Support', 'custom_dashboard_help');
+}
+add_action('wp_dashboard_setup', 'wts_custom_dashboard_widgets');
+
+
+/*  NAVIGATION
+________________________________________________________________________*/
+
+function eg_register_menus() {
+	register_nav_menus(
+  		array(
+			'header_nav_menu' => __( 'Header Menu' ),
+			'footer_nav_menu' => __( 'Footer Menu' ),
+         'footer_alt_menu' => __( 'Alternate Menu' ),
+         'content_altTwo_menu' => __( 'Alternate Menu - 2' ),
+         'content_altThr_menu' => __( 'Alternate Menu - 3' ),
+         'content_altFou_menu' => __( 'Alternate Menu - 4' ),
+         'content_altFiv_menu' => __( 'Alternate Menu - 5' ),
+    	)
+	);
+}
+add_action( 'init', 'eg_register_menus' );
+
+
+function cleanname($v) {
+$v = preg_replace('/[^a-zA-Z0-9s]/', '', $v);
+$v = str_replace(' ', '-', $v);
+$v = strtolower($v);
+return $v;
+}
+
+
+
+/*  PLUGIN EDITS
+________________________________________________________________________*/
+
+/*  Yoast
+__________________________________________*/
+
+// Disable Yoast SEO Primary Category Feature
+add_filter( 'wpseo_primary_term_taxonomies', '__return_false' );
+
+// Moves Yoast below Content Editor
+function yoasttobottom() {
+  return 'low';
+}
+add_filter( 'wpseo_metabox_prio', 'yoasttobottom');
+
+/*  Tablepress
+__________________________________________*/
+
+// Removes the Tablepress Admin links on site
+add_filter( 'tablepress_edit_link_below_table', '__return_false' );
+
+/*  GRAVITY FORMS
+__________________________________________*/
+
+// keeps the viewer at the form to read the confirmation message
+// instead of having to scroll to message
+add_filter( 'gform_confirmation_anchor', '__return_true' );
+
+// Hides top labels if Placeholders are added - dropdown option
+add_filter( 'gform_enable_field_label_visibility_settings', '__return_true' );
+
+
+/*  LAZY LOAD
+________________________________________________________________________*/
+
+
+
+
+
+/* THIS IS THE END                                                       */
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
+/* --------------------------------------------------------------------- */
---
+++ readme.txt
@@ -0,0 +1,23 @@
+=== WTS Elementor ===
+
+Contributors: Thomas Rainer
+Requires at least: 4.7
+Tested up to: 6.1
+Stable tag: 0.1.0
+Version: 0.1.0
+Requires PHP: 7.4
+JQuery: 3.6.1
+License: GNU General Public License v3 or later
+License URI: https://www.gnu.org/licenses/gpl-3.0.html
+
+A lightweight, plain-vanilla theme for Elementor page builder.
+
+== Description ==
+
+A basic, plain-vanilla, lightweight theme, best suited for building your site using Elementor page builder.
+This theme resets the WordPress environment and prepares it for smooth operation of Elementor and LifterLMS.
+
+Screenshot's images & icons are licensed under: Creative Commons (CC0), https://creativecommons.org/publicdomain/zero/1.0/legalcode
+
+= 0.1.0 - 12-16-2022 =
+* Initial Release
